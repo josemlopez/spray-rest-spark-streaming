05b401567d8cf5fb9aea665162284605f4af2352
josemlopezm <josem.lopezm@gmail.com>
2015-11-02 12:58:53 +0100

    Changes in test. This test will be changed again

--- src/test/scala/com/jmlopezm/routing/RestRoutingSpec.scala
+++ src/test/scala/com/jmlopezm/routing/RestRoutingSpec.scala
@@ -6,23 +6,19 @@ import org.scalatest.{Matchers, FlatSpec}
 import spray.routing._
 import spray.testkit.ScalatestRouteTest
 
-class RestRoutingSpec extends FlatSpec with ScalatestRouteTest with Matchers {
+class RestRoutingSpec extends FlatSpec with ScalatestRouteTest with Matchers{
 
-  val petsWithOwnerService = TestProbe()
+  val sparkService = TestProbe()
 
   def restRouting = TestActorRef(new RestSpark() {
-    override def petsWithOwner(message : RestMessage): Route =
-      ctx => perRequestSupervisorActor(ctx, petsWithOwnerService.ref, message)
+    override def sparkRoute(message : RestMessage): Route =
+      ctx => perRequestSupervisorActorCreator(ctx, sparkService.ref, message)
   })
 
-	"RestRouting" should "get Pets" in {
-    val getAnimals = Get("/pets?names=PetName") ~> restRouting.underlyingActor.route
-
-    petsWithOwnerService.expectMsg(GetPetsWithOwners("PetName" :: Nil))
-    petsWithOwnerService.reply(SparkAndEvent(SparkIDForEvent("PetName", SparkReference("OwnerName")) :: Nil))
-
-    getAnimals ~> check {
-      responseAs[String] should equal("""{"pets":[{"name":"PetName","owner":{"name":"OwnerName"}}]}""")
+  "The Post Service" should "return 'bien' for POST request on /" in {
+    Post("/", """{"idService": "id1", "line" : "string to check"}"""   ) ~> restRouting.underlyingActor.route ~> check{
+      responseAs[String] should be ("bien")
     }
   }
+
 }
