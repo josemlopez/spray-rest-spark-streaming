e77702fb0ca30b7ef1a0690dceea3635269d921a
josemlopezm <josem.lopezm@gmail.com>
2015-11-02 12:57:13 +0100

    Include some logs and change timeOut for testing

--- src/main/scala/com/jmlopezm/core/EventManagerActor.scala
+++ src/main/scala/com/jmlopezm/core/EventManagerActor.scala
@@ -1,6 +1,6 @@
 package com.jmlopezm.core
 
-import akka.actor.{Actor, ActorRef}
+import akka.actor.{ActorLogging, Actor, ActorRef}
 import com.jmlopezm.{SparkReference, _}
 import com.jmlopezm.clients.MetricsActor.AddEvent
 import com.jmlopezm.clients.ResourcesActor.{GetSparkCluster, SparkForId}
@@ -10,13 +10,14 @@ import com.jmlopezm.domain.Event
  * This actor is the center of the action. He is in charge of asking about the Spark where to send the event and send if
  * if is possible
  */
-class EventManagerActor(metricsActor: ActorRef, resourcesActor: ActorRef) extends Actor {
+class EventManagerActor(metricsActor: ActorRef, resourcesActor: ActorRef) extends Actor with ActorLogging {
   
   var sparkRef: Option[SparkReference] = Option.empty[SparkReference]
   var eventToSend: Option[Event] = Option.empty[Event]
 
   def receive = {
     case SentEventToSpark(event) =>
+      log.debug("We'll get the SparkID and send the message to the Actor of this Spark")
       eventToSend = Some(event)
       resourcesActor ! GetSparkCluster(event.idService)
       context.become(waitingForSparkID)
@@ -24,7 +25,8 @@ class EventManagerActor(metricsActor: ActorRef, resourcesActor: ActorRef) extend
 
   def waitingForSparkID: Receive = {
     case SparkForId(sparkReference) =>
-      sparkRef = Some(sparkReference)
+      sparkRef = sparkReference
+      log.debug("It appear that we have the reference and here it's: " + sparkRef)
       sendEventWhenPossible
 
     case f: Validation => context.parent ! f
@@ -34,7 +36,11 @@ class EventManagerActor(metricsActor: ActorRef, resourcesActor: ActorRef) extend
     if(sparkRef.nonEmpty) {
       val enrichedRes: SparkIDForEvent = SparkIDForEvent(eventToSend.get, sparkRef.get)
       metricsActor ! AddEvent()
-      print ("Here we send the event to : " + sparkRef.get.name)
+      print ("We have sent the event to : " + sparkRef.get.name)
       context.parent ! SparkAndEvent(enrichedRes)
+    } else {
+      val errorMessage = "There is no Spark Actor for this ID: " + eventToSend.get.idService
+      log.debug(errorMessage)
+      context.parent ! Error(errorMessage)
     }
 }
